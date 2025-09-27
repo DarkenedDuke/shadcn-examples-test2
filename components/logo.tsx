7b07253626dce108bbea1f3ee59b1b9e5edd7df3
Dionizen Geisler Bino <152365462+DarkenedDuke@users.noreply.github.com>
2025-09-27 18:11:16 +0800

    Update logo.tsx

--- components/logo.tsx
+++ components/logo.tsx
@@ -11,7 +11,6 @@ export function Logo() {
           alt="shadcn examples logo"
         />
       </div>
-      <span className="hidden font-medium md:inline">Jerome Escultura</span>
       <span className="hidden font-medium md:inline">Dionizen Geisler Bino</span>
     </div>
   );
